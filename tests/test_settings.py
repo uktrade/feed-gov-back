75d40529f914b7c76d1ae174fcdb64cd80f4110d
christopherpettinga <chrispettinga@gmail.com>
2024-02-02 09:25:22 +0000

    Fixing test settings
    Upgrading to django 4.2.x
    
    Fixing JSONField errors

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -11,12 +11,8 @@ INSTALLED_APPS = [
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST', 'localhost'),
-        'PORT': os.environ.get('DB_PORT', 5432)
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': "testdb",
     },
 }
 
